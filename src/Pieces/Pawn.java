3373c3a855eb0f6cbdd13821028879f8154c23e3
Rayhan Heru Arifiansyah <rayhanheruk@gmail.com>
2024-07-20 11:43:11 +0700

    add pawn movement

--- src/Pieces/Pawn.java
+++ src/Pieces/Pawn.java
@@ -18,4 +18,29 @@ public class Pawn extends Piece {
         this.sprite = sheet.getSubimage(5 * sheetScale, isWhite ? 0 : sheetScale, sheetScale, sheetScale)
                 .getScaledInstance(board.tileSize, board.tileSize, BufferedImage.SCALE_SMOOTH);
     }
+
+    public boolean isValidMovement(int col, int row) {
+
+        int colorIndex = isWhite ? 1 : -1;
+
+        if (this.col == col && row == this.row - colorIndex && board.getPiece(col, row) == null) {
+            return true;
+        }
+
+        if (isFirstMove && this.col == col && row == this.row - colorIndex * 2 && board.getPiece(col, row) == null
+                && board.getPiece(col, row + colorIndex) == null) {
+            return true;
+        }
+
+        if (col == this.col - 1 && row == this.row - colorIndex && board.getPiece(col, row) != null) {
+            return true;
+        }
+
+        if (col == this.col + 1 && row == this.row - colorIndex && board.getPiece(col, row) != null) {
+            return true;
+        }
+
+        return false;
+
+    }
 }
